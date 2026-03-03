2c49dcc52f0ff2452841977926d7bbd546248fc0
Chinonso Emmanuel <emmanuelchinons9@gmail.com>
2026-03-03 08:52:37 +0100

    fix tailwind config error

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      black: "#1a202c",
+    extend: {
+      colors: {
+        black: "#1a202c",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
