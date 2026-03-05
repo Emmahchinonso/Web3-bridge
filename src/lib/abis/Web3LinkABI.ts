f8a80273daedc5d192b8673257317273faded4b5
Chinonso Emmanuel <emmanuelchinons9@gmail.com>
2026-03-05 07:40:39 +0100

    Add  contract service

---
+++ src/lib/abis/Web3LinkABI.ts
@@ -0,0 +1,99 @@
+export default [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "claimStarterAssets",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAssets",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "id",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Web3LinkAssets.Asset[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "hasClaimed",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "assetId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transferAsset",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
--- src/services/ContractService.ts
+++ src/services/ContractService.ts
@@ -1,3 +1,19 @@
-class ContractService {}
+import type {
+  Interface,
+  InterfaceAbi,
+  JsonRpcApiProvider,
+  JsonRpcSigner,
+} from "ethers";
+import { Contract } from "ethers";
+
+class ContractService {
+  getContract(
+    contractAddress: string,
+    abi: Interface | InterfaceAbi,
+    provider: JsonRpcSigner | JsonRpcApiProvider
+  ) {
+    return new Contract(contractAddress, abi, provider);
+  }
+}
 
 export const contractService = new ContractService();
